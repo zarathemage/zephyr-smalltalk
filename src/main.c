160c822966e7d3544b11822d025afd787840cdb4
zarathemage <74919281+zarathemage@users.noreply.github.com>
2021-05-31 01:18:51 +0200

    Add files via upload
    start of vm

--- src/main.c
+++ src/main.c
@@ -10,10 +10,12 @@ int main(int argc, char *argv[])
 	SDL_Init(SDL_INIT_EVERYTHING);	
 
 	ZSController c;
+	ZSView view;
 
-	c.view = (ZSViewPtr)malloc(sizeof ZSView);
-	c.view->screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE); 
-
+	SDL_Surface *scr = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE); 
+	view->screen = scr; 
+	c.view = &view;
+	
 	smalltalk_loop(&zsc);
 
 	return 0;
---
+++ src/pragmas.h
@@ -0,0 +1,10 @@
+#ifndef __PRAGMAS_H_
+#define __PRAGMAS_H_
+
+// vm opcodes
+#define ZVM_PLUS 0x0001
+#define ZVM_MINUS 0x0002
+
+// native types
+
+#endif
