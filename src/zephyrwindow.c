c49130d7b0a2cd3f1c69c21f0323792870075b6c
zarathemage <74919281+zarathemage@users.noreply.github.com>
2021-06-01 03:36:41 +0200

    Add files via upload

--- src/zephyrwindow.c
+++ src/zephyrwindow.c
@@ -5,8 +5,29 @@ ZephyrWindowPtr make_zephyr_window(int x, int y, int w, int h, ZephyrWindowPtr z
 	zw = (ZephyrWindowPtr)malloc(sizeof ZephyrWindowPtr);
 	zw->zs = make_zephyr_structure(x,y,w,h);
 	zw->zbh = make_zephyr_behaviour(rand_util_gen_b_id(x+y));
-	
-	return zw;
+
+/* Create a 32-bit surface with the bytes of each pixel in R,G,B,A order,
+       as expected by OpenGL for textures */ 
+    SDL_Surface *surface;
+    Uint32 rmask, gmask, bmask, amask;
+    
+    /* SDL interprets each pixel as a 32-bit number, so our masks must depend
+       on the endianness (byte order) of the machine */ 
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    rmask = 0xff000000;
+    gmask = 0x00ff0000;
+    bmask = 0x0000ff00;
+    amask = 0x000000ff;
+#else
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0xff000000;
+#endif
+
+    zw->surface = SDL_CreateRGBSurface(0, zw->zs->width, zw->zs->height, 32,
+                                   rmask, gmask, bmask, amask);	
+    return zw;
 }
 	
 void delete_zephyr_window(ZephyrWindowPtr zw)
@@ -19,6 +40,17 @@ void move_zehpyr_window(int x, int y, ZephyrWindowPtr zw)
 {
 	zw->zs->x = x;
 	zw->zs->y = y;
+}
+
+void paint_zephyr_window(SDL_Surface *screen, ZephyrWindowPtr zw)
+{
+
+	SDL_Rect rect;
+	rect.x = zw->zs->x; 
+	rect.y = zw->zs->y; 
+
+	SDL_BlitSurface(zw->surface, 0, screen, rect);
+	SDL_Flip(); // NOTE FIXME put in seperate method ?
 }
 
 void write_out_zephyr_window(long int offset, ZephyrStructurePtr zs, ZephyrBehaviourPtr zbh, ImageBufPtr buf)
--- src/zephyrwindow.h
+++ src/zephyrwindow.h
@@ -1,12 +1,15 @@
 #ifndef __ZEPHYR_WINDOW_H_
 #define __ZEPHYR_WINDOW_H_
 
+#include <SDL.h>
+
 #include "zephyrstructure.h"
 #include "zephyrbehaviour.h"
 
 typedef struct zephyr_window {
   ZephyrStructurePtr zs;
   ZephyrBehaviourPtr zbh;
+  SDL_Surface *surface;
 } ZephyrWindow;
 
 typedef ZephyrWindow *ZephyrWindowPtr;	
@@ -15,6 +18,8 @@ ZephyrWindowPtr make_zephyr_window(int x, int y, int w, int h, ZephyrWindowPtr z
 void delete_zephyr_window(ZephyrWindowPtr zw);
 
 void move_zehpyr_window(int x, int y, ZephyrWindowPtr zw);
+
+void paint_zephyr_window(SDL_Surface *screen, ZephyrWindowPtr zw);
 
 void write_out_zephyr_window(long int offset, ZephyrStructurePtr zs, ZephyrBehaviourPtr zbh, ImageBufPtr buf);
 
